959741a3e718b31263c61ea672c0632aadd53bf4
XuDong <264835229@qq.com>
2018-06-07 17:51:26 +0800

    解释器

---
+++ src/com/xudong/BigTalk/DesignPattern/Interpreter/AbstractExpression.java
@@ -0,0 +1,6 @@
+package com.xudong.BigTalk.DesignPattern.Interpreter;
+
+public abstract class AbstractExpression {
+
+	public abstract void interpret(Context context);
+}
---
+++ src/com/xudong/BigTalk/DesignPattern/Interpreter/TerminalExpression.java
@@ -0,0 +1,10 @@
+package com.xudong.BigTalk.DesignPattern.Interpreter;
+
+public class TerminalExpression extends AbstractExpression {
+
+	@Override
+	public void interpret(Context context) {
+		System.out.println("终端解释器！");
+	}
+
+}
